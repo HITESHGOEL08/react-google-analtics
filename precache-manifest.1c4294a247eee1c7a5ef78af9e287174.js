01157c202c205e5ab384d11d106e30977820118c
HITESHGOEL08 <hiteshgoel426@gmail.com>
2020-08-19 20:24:40 +0530

    Updates

---
+++ precache-manifest.1c4294a247eee1c7a5ef78af9e287174.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bfa7bcaf29ad5ee8cc11602ae87362",
+    "url": "/react-google-analtics/index.html"
+  },
+  {
+    "revision": "1eb2c94e5186ea6028f5",
+    "url": "/react-google-analtics/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/react-google-analtics/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-google-analtics/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb2c94e5186ea6028f5",
+    "url": "/react-google-analtics/static/js/main.71c81e88.chunk.js"
+  },
+  {
+    "revision": "afe0bfd7556368985b97",
+    "url": "/react-google-analtics/static/js/runtime-main.aed1694e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-google-analtics/static/media/logo.5d5d9eef.svg"
+  }
+]);
